3d65b13ebc853ca2da88734b266d6137234958b9
Luana Ferreira <85973402+efluana@users.noreply.github.com>
2022-02-22 12:15:38 -0300

    Add files via upload

---
+++ aula-16.02---TA-ds-main/aula01/controle/function.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $('.btn-send').click(function(e){
+
+        e.preventDefault();
+
+        let= dados = $ ('#form').serialize()
+
+        console.log(dados)
+    })
+})
